e886b0361f7ca274639922232078300b1e82f453
IDM.air <innanadm8@gmail.com>
2019-06-27 17:19:38 -0400

    creating...

---
+++ server.js
@@ -0,0 +1,29 @@
+
+// first thing is require all dependencies, node packages
+var express = require("express");
+
+// ?is morgan a middle man for db and front end? why is it called a logger?
+var logger = require("morgan");
+
+var mongoose = require("mongoose");
+
+
+// ?I can't figure out how to find port...do I have to set my own port? I tried running node server.js but I'm still not getting a port
+var PORT = 3030;
+
+// path to models folder and every thing in it
+var db = require("./models");
+
+
+// initializes express, also referenced npm express doc
+var app = express();
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
